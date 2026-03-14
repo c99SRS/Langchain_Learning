f235d0e244f261a2878111b94b37803d78b12176
c99srs <smruti.work@gmail.com>
2026-03-14 16:25:28 +0530

    added 2 tools

---
+++ agent-under-the-hood/1_agent_loop_langchain_tool_calling.py
@@ -0,0 +1,67 @@
+
+from dotenv import load_dotenv
+from langchain_core import messages
+
+load_dotenv()
+
+from langchain.chat_models import init_chat_model
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage , SystemMessage, ToolMessage
+from langsmith import traceable
+
+MAX_ITERATION=10
+MODEL = "qwen3.5:0.8b"
+
+# tool decorator
+
+@tool
+def get_product_price(product:str) -> float:
+    """ Look up the price of a product in the catalog """
+    print(f" >> Executing get_product_price(product='{product}')")
+    prices={"laptop":1299.99, "headphones":149.95, "keyboard":89.50}
+    return prices.get(product,0)
+
+
+@tool
+def apply_discount(price:float, discount_tier:str) -> float:
+    """ Apply a discount tier to a price and return the final price. Available tier: silver,gold, bronze """
+    print(f" >> Executing apply_discount(price='{price}', discount_tier='{discount_tier}')")
+    discount_percentage={"bronze":5, "silver":12,"gold":23}
+    discount = discount_percentage.get(discount_percentage,0)
+    return round(price * (1- discount/100),2)
+
+
+@traceable(name="langchain Agent Loop")
+def run_agent(question:str):
+    tools = [get_product_price, apply_discount]
+    tools_dict = {t.name: t for t in tools}
+
+    llm = init_chat_model(f"ollama:{MODEL}", temperature=0)
+    #now bind the llm with tools with bindtools()
+    llm_with_tools = llm.bind_tools(tools)
+
+    print(f"Question: {question}")
+    print("=" * 60)
+
+    messages = [
+        SystemMessage(content=("You are a helpful shopping Assistant."
+                                "You have access to product catalog tool"
+                                "and a discount tool. \n\n"
+                                "STRICT RULES- you must follow these exactly:\n"
+                                "1. Never guess or assume any product price."
+                                "You must call get_product_price first to get the real price. \n"
+                                "2. Only call apply_discount AFTER you have received a price from get_product_price. pass th eexact price"
+                                "returned by get_product_price - do NOT pass a made-up number. \n"
+                                "3. NEVER calculate discounts yourself using math."
+                                "Always use the apply_discount tool \n"
+                                "4. If the user does not specify a discount tier, ask them which tier to use - do NOT assume one."
+                                ) ), 
+        HumanMessage(content=question)
+    ]
+
+
+if __name__ == "__main__":
+    print("Hello Langchain Agent (.bind_tools)!")
+    print()
+    result = run_agent("what is the price of a laptop after applying a gold discount?")
+
